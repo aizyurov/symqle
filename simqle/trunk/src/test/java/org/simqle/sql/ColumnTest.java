9fcffdab362beade3e9ecd242a0aacd2dff146b9
Alexander Izyurov <aizyurov@symqle.org>
2012-11-21 20:40:16 +0000

    a few tests added

--- simqle/trunk/src/test/java/org/simqle/sql/ColumnTest.java
+++ simqle/trunk/src/test/java/org/simqle/sql/ColumnTest.java
@@ -159,6 +159,14 @@ public class ColumnTest extends TestCase {
 
     }
 
+    public void testUseSameTableInDistinct() throws Exception {
+        final LongColumn column = createId();
+        final LongColumn age = createAge();
+        final String sql = column.intersectDistinct(age).show();
+        assertEquals("SELECT T0.id AS C0 FROM person AS T0 INTERSECT DISTINCT SELECT T1.age AS C0 FROM person AS T1", sql);
+
+    }
+
     private static class Person extends Table {
         private Person() {
             super("person");
@@ -189,6 +197,26 @@ public class ColumnTest extends TestCase {
         public String value(final Element element) throws SQLException {
             return element.getString();
         }
+    }
+
+    public void testSelectForUpdate() throws Exception {
+        final LongColumn col = createId();
+        assertEquals("SELECT T0.id AS C0 FROM person AS T0 FOR UPDATE", col.forUpdate().show());
+    }
+
+    public void testSelectForReadOnly() throws Exception {
+        final LongColumn col = createId();
+        assertEquals("SELECT T0.id AS C0 FROM person AS T0 FOR READ ONLY", col.forReadOnly().show());
+    }
+
+    public void testSelectExists() throws Exception {
+        final LongColumn id  =  createId();
+        final LongColumn age = createAge();
+        // find all but the most old
+        final LongColumn age2 = new LongColumn("age", person2);
+        String sql = id.where(age2.where(age2.gt(age)).exists()).show();
+        assertEquals("SELECT T0.id AS C0 FROM person AS T0 where exists (select T1.age FROM person T1 where T1.age > T0.age", sql);
+
     }
 
 }
