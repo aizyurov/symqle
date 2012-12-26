d530796f36a438a31f5c9227b61d38a487568153
Alexander Izyurov <aizyurov@symqle.org>
2012-12-26 11:54:07 +0000

    CursorSpecification and SelectStatement tests

---
+++ simqle-0.1/trunk/src/test/java/org/simqle/sql/SelectStatementTest.java
@@ -0,0 +1,87 @@
+package org.simqle.sql;
+
+import org.simqle.Callback;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+
+/**
+ * @author lvovich
+ */
+public class SelectStatementTest extends SqlTestCase {
+
+
+    public void testList() throws Exception {
+        final DataSource datasource = createMock(DataSource.class);
+        final Connection connection = createMock(Connection.class);
+        final PreparedStatement statement = createMock(PreparedStatement.class);
+        final ResultSet resultSet = createMock(ResultSet.class);
+        final String queryString = person.id.select().forUpdate().show();
+        expect(datasource.getConnection()).andReturn(connection);
+        expect(connection.prepareStatement(queryString)).andReturn(statement);
+        expect(statement.executeQuery()).andReturn(resultSet);
+        expect(resultSet.next()).andReturn(true);
+        expect(resultSet.getLong(matches("C[0-9]"))).andReturn(123L);
+        expect(resultSet.wasNull()).andReturn(false);
+        expect(resultSet.next()).andReturn(false);
+        resultSet.close();
+        statement.close();
+        connection.close();
+        replay(datasource, connection,  statement, resultSet);
+
+        final List<Long> list = person.id.select().forUpdate().list(datasource);
+        assertEquals(1, list.size());
+        assertEquals(123L, list.get(0).longValue());
+        verify(datasource, connection, statement, resultSet);
+    }
+
+
+    public void testScroll() throws Exception {
+        final DataSource datasource = createMock(DataSource.class);
+        final Connection connection = createMock(Connection.class);
+        final PreparedStatement statement = createMock(PreparedStatement.class);
+        final ResultSet resultSet = createMock(ResultSet.class);
+        final String queryString = person.id.select().forUpdate().show();
+        expect(datasource.getConnection()).andReturn(connection);
+        expect(connection.prepareStatement(queryString)).andReturn(statement);
+        expect(statement.executeQuery()).andReturn(resultSet);
+        expect(resultSet.next()).andReturn(true);
+        expect(resultSet.getLong(matches("C[0-9]"))).andReturn(123L);
+        expect(resultSet.wasNull()).andReturn(false);
+        expect(resultSet.next()).andReturn(false);
+        resultSet.close();
+        statement.close();
+        connection.close();
+        replay(datasource, connection,  statement, resultSet);
+
+        person.id.select().forUpdate().scroll(datasource, new Callback<Long, SQLException>() {
+            int callCount = 0;
+
+            @Override
+            public void iterate(final Long aNumber) throws SQLException, BreakException {
+                if (callCount++ != 0) {
+                    fail("One call expected, actually " + callCount);
+                }
+                assertEquals(123L, aNumber.longValue());
+            }
+        });
+        verify(datasource, connection,  statement, resultSet);
+    }
+
+
+    private static class Person extends Table {
+        private Person() {
+            super("person");
+        }
+        public Column<Long> id = new LongColumn("id", this);
+    }
+
+    private static Person person = new Person();
+
+}
