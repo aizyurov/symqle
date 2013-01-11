a1d2ee2ac4ce01b5e232c4c9a4d406b953e6ec96
Alexander Izyurov <aizyurov@symqle.org>
2013-01-11 10:42:34 +0000

    update tests complete

--- simqle-0.1/trunk/src/test/java/org/simqle/coretest/UpdateTest.java
+++ simqle-0.1/trunk/src/test/java/org/simqle/coretest/UpdateTest.java
@@ -1,8 +1,18 @@
 package org.simqle.coretest;
 
 import org.simqle.Mappers;
+import org.simqle.sql.AbstractUpdateStatement;
+import org.simqle.sql.AbstractUpdateStatementBase;
 import org.simqle.sql.Column;
 import org.simqle.sql.Table;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
 
 /**
  * @author lvovich
@@ -24,6 +34,11 @@ public class UpdateTest extends SqlTestCase {
         assertSimilar("UPDATE person SET parent_id = DEFAULT", sql);
     }
 
+    public void testSetIgnoreType() throws Exception {
+        final String sql = person.update(person.id.setIgnoreType(person.id.plus(1))).show();
+        assertSimilar("UPDATE person SET id = person.id + ?", sql);
+    }
+
     public void testMultipleColumns() throws Exception {
         final String sql = person.update(person.parentId.set(person.id), person.name.set("John")).show();
         assertSimilar("UPDATE person SET parent_id = person.id, name = ?", sql);
@@ -35,10 +50,85 @@ public class UpdateTest extends SqlTestCase {
     }
 
     public void testSubqueryInWhere() throws Exception {
-        final Person children = new Person();
-        final String sql = person.update(person.parentId.set(person.id)).where(children.id.where(children.parentId.eq(person.id)).exists()).show();
+        final Person child = new Person();
+        final String sql = person.update(person.parentId.set(person.id)).where(child.id.where(child.parentId.eq(person.id)).exists()).show();
         assertSimilar("UPDATE person SET parent_id = person.id WHERE EXISTS(SELECT T0.id FROM person AS T0 WHERE T0.parent_id = person.id)", sql);
     }
+
+    public void testSubqueryAsSource() throws Exception {
+        final Person child = new Person();
+        final String sql = person.update(person.name.set(child.name.where(child.parentId.eq(person.id)).queryValue())).show();
+        assertSimilar("UPDATE person SET name =(SELECT T0.name FROM person AS T0 WHERE T0.parent_id = person.id)", sql);
+    }
+
+    public void testSubqueryFromNoTables() throws Exception {
+        try {
+            final String sql = person.update(person.id.set(person.parentId.where(person.id.eq(1L)).queryValue())).show();
+            fail("IllegalStateException expected but was " + sql);
+        } catch (IllegalStateException e) {
+            assertTrue(e.getMessage(), e.getMessage().contains("does not support"));
+        }
+    }
+
+    public void testWrongTarget() throws Exception {
+        final Person child = new Person();
+        try {
+            final String sql = person.update(child.name.set(person.name)).show();
+            fail("IllegalArgumentException expected, but was " + sql);
+        } catch (IllegalArgumentException e) {
+            // fine
+            assertTrue(e.getMessage().contains("is not legal in this context"));
+        }
+    }
+
+    public void testWrongSource() throws Exception {
+        final Person child = new Person();
+        try {
+            final String sql = person.update(person.name.set(child.name)).show();
+            fail("IllegalArgumentException expected, but was " + sql);
+        } catch (IllegalArgumentException e) {
+            // fine
+            assertTrue(e.getMessage().contains("is not legal in this context"));
+        }
+    }
+
+    public void testExecute() throws Exception {
+        final AbstractUpdateStatementBase update = person.update(person.name.set("John"));
+        final String statementString = update.show();
+        final DataSource datasource = createMock(DataSource.class);
+        final Connection connection = createMock(Connection.class);
+        final PreparedStatement statement = createMock(PreparedStatement.class);
+        expect(datasource.getConnection()).andReturn(connection);
+        expect(connection.prepareStatement(statementString)).andReturn(statement);
+        statement.setString(1, "John");
+        expect(statement.executeUpdate()).andReturn(2);
+        statement.close();
+        connection.close();
+        replay(datasource, connection,  statement);
+
+        assertEquals(2, update.execute(datasource));
+
+    }
+
+    public void testExecuteSearched() throws Exception {
+        final AbstractUpdateStatement update = person.update(person.name.set("John")).where(person.id.eq(1L));
+        final String statementString = update.show();
+        final DataSource datasource = createMock(DataSource.class);
+        final Connection connection = createMock(Connection.class);
+        final PreparedStatement statement = createMock(PreparedStatement.class);
+        expect(datasource.getConnection()).andReturn(connection);
+        expect(connection.prepareStatement(statementString)).andReturn(statement);
+        statement.setString(1, "John");
+        statement.setLong(2, 1L);
+        expect(statement.executeUpdate()).andReturn(1);
+        statement.close();
+        connection.close();
+        replay(datasource, connection,  statement);
+
+        assertEquals(1, update.execute(datasource));
+
+    }
+
 
     private static class Person extends Table {
         private Person() {
