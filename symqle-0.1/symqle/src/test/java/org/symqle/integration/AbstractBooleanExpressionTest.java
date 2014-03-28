0dfd487f8f5bd6d40f080d3298aba8006eb85f3b
Alexander Izyurov <aizyurov@symqle.org>
2014-03-28 10:43:11 +0000

    symqle tests refactoring

---
+++ symqle-0.1/symqle/src/test/java/org/symqle/integration/AbstractBooleanExpressionTest.java
@@ -0,0 +1,278 @@
+package org.symqle.integration;
+
+import org.symqle.common.Pair;
+import org.symqle.integration.model.Employee;
+import org.symqle.integration.model.InsertTable;
+import org.symqle.sql.AbstractBooleanExpression;
+import org.symqle.sql.BooleanExpression;
+import org.symqle.sql.Params;
+import org.symqle.testset.AbstractBooleanExpressionTestSet;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author lvovich
+ */
+public class AbstractBooleanExpressionTest extends AbstractIntegrationTestBase implements AbstractBooleanExpressionTestSet {
+
+    /**
+     * Returns condition, which is true for ["Cooper", "First", "Pedersen"]
+     * @param employee
+     * @return
+     */
+    private AbstractBooleanExpression createBasicCondition(final Employee employee) {
+        return employee.retired.asPredicate().or(employee.hireDate.ge(new Date(108, 9, 1)));
+    }
+
+    @Override
+    public void test_adapt_BooleanExpression() throws Exception {
+        final Employee employee = new Employee();
+        final BooleanExpression be = employee.hireDate.ge(new Date(108, 9, 1));
+        final List<String> list = employee.lastName.where(AbstractBooleanExpression.adapt(be).and(employee.salary.gt(2500.0))).list(getEngine());
+        assertEquals(Arrays.asList("First"), list);
+    }
+
+    @Override
+    public void test_and_BooleanFactor() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(basicCondition.and(employee.salary.gt(2500.0))).list(getEngine());
+        assertEquals(Arrays.asList("First"), list);
+    }
+
+    @Override
+    public void test_and_BooleanTerm_BooleanFactor_1() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(employee.salary.gt(2500.0).and(basicCondition)).list(getEngine());
+        assertEquals(Arrays.asList("First"), list);
+    }
+
+    @Override
+    public void test_asValue_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<Pair<String, Boolean>> list = employee.lastName.pair(basicCondition.asValue()).
+                orderBy(employee.lastName).
+                list(getEngine());
+        assertEquals(Arrays.asList(
+                        Pair.make("Cooper", true),
+                        Pair.make("First", true),
+                        Pair.make("March", false),
+                        Pair.make("Pedersen", true),
+                        Pair.make("Redwood", false)),
+                        list);
+    }
+
+    @Override
+    public void test_isFalse_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isFalse())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(Arrays.asList("March", "Redwood"), list);
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "FALSE" at line 1, column 92
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_isNotFalse_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isNotFalse())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(Arrays.asList("Cooper", "First", "Pedersen"), list);
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "TRUE" at line 1, column 92
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_isNotTrue_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isNotTrue())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(Arrays.asList("March", "Redwood"), list);
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "TRUE" at line 1, column 96.
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_isNotUnknown_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isNotUnknown())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(5, list.size());
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "UNKNOWN" at line 1, column 96.
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_isTrue_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isTrue())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(Arrays.asList("Cooper", "First", "Pedersen"), list);
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "TRUE" at line 1, column 92
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_isUnknown_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        try {
+            final List<String> list = employee.lastName.where(basicCondition.isUnknown())
+                    .orderBy(employee.lastName)
+                    .list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // derby: ERROR 42X01: Syntax error: Encountered "UNKNOWN" at line 1, column 92.
+            expectSQLException(e, "Apache Derby");
+        }
+    }
+
+    @Override
+    public void test_negate_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(basicCondition.negate()).orderBy(employee.lastName).list(getEngine());
+        assertEquals(Arrays.asList("March", "Redwood"), list);
+    }
+
+    public void testOr() throws Exception {
+    }
+
+    @Override
+    public void test_or_BooleanExpression_BooleanTerm_1() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(employee.firstName.eq("Bill").or(basicCondition))
+                .orderBy(employee.lastName)
+                .list(getEngine());
+        assertEquals(Arrays.asList("Cooper", "First", "March", "Pedersen"), list);
+    }
+
+    @Override
+    public void test_or_BooleanTerm() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(basicCondition.or(employee.firstName.eq("Bill")))
+                .orderBy(employee.lastName)
+                .list(getEngine());
+        assertEquals(Arrays.asList("Cooper", "First", "March", "Pedersen"), list);
+    }
+
+    @Override
+    public void test_thenNull_() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<Pair<String,String>> list = employee.lastName.pair(
+                    employee.salary.gt(2500.0).then(employee.firstName).orWhen(basicCondition.thenNull()).orElse(Params.p(":)"))
+                )
+                .orderBy(employee.lastName).list(getEngine());
+        assertEquals(Arrays.asList(Pair.make("Cooper", null), Pair.make("First", "James"), Pair.make("March", ":)"), Pair.make("Pedersen", null), Pair.make("Redwood", "Margaret")), list);
+    }
+
+    @Override
+    public void test_then_ValueExpression() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<Pair<String,String>> list = employee.lastName.pair(basicCondition.then(employee.firstName))
+                .orderBy(employee.lastName).list(getEngine());
+        assertEquals(Arrays.asList(Pair.make("Cooper", "James"), Pair.make("First", "James"), Pair.make("March", null), Pair.make("Pedersen", "Alex"), Pair.make("Redwood", null)), list);
+    }
+
+    @Override
+    public void test_where_DeleteStatementBase_WhereClause_1() throws Exception {
+        final InsertTable insertTable = new InsertTable();
+        prepareTestData(insertTable);
+        final AbstractBooleanExpression abstractBooleanExpression =
+                insertTable.id.gt(4).or(insertTable.text.like("t%"));
+        insertTable.delete().where(abstractBooleanExpression).execute(getEngine());
+        final List<Integer> list = insertTable.id.orderBy(insertTable.id).list(getEngine());
+        assertEquals(Arrays.asList(1,4), list);
+
+    }
+
+    private void prepareTestData(final InsertTable insertTable) throws SQLException {
+        insertTable.delete().execute(getEngine());
+        insertTable.insert(insertTable.id.set(1).also(insertTable.text.set("one"))).execute(getEngine());
+        insertTable.insert(insertTable.id.set(2).also(insertTable.text.set("two"))).execute(getEngine());
+        insertTable.insert(insertTable.id.set(3).also(insertTable.text.set("tree"))).execute(getEngine());
+        insertTable.insert(insertTable.id.set(4).also(insertTable.text.set("four"))).execute(getEngine());
+        insertTable.insert(insertTable.id.set(5).also(insertTable.text.set("five"))).execute(getEngine());
+    }
+
+    @Override
+    public void test_where_QueryBaseScalar_WhereClause_1() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<String> list = employee.lastName.where(basicCondition).
+                orderBy(employee.lastName).
+                list(getEngine());
+        assertEquals(Arrays.asList(
+                        "Cooper",
+                        "First",
+                        "Pedersen"),
+                        list);
+    }
+
+    @Override
+    public void test_where_QueryBase_WhereClause_1() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractBooleanExpression basicCondition = createBasicCondition(employee);
+        final List<Pair<String, Boolean>> list = employee.lastName.pair(basicCondition.asValue()).
+                where(basicCondition).
+                orderBy(employee.lastName).
+                list(getEngine());
+        assertEquals(Arrays.asList(
+                        Pair.make("Cooper", true),
+                        Pair.make("First", true),
+                        Pair.make("Pedersen", true)),
+                        list);
+    }
+
+    @Override
+    public void test_where_UpdateStatementBase_WhereClause_1() throws Exception {
+        final InsertTable insertTable = new InsertTable();
+        prepareTestData(insertTable);
+        final AbstractBooleanExpression abstractBooleanExpression =
+                insertTable.id.gt(4).or(insertTable.text.like("t%"));
+        insertTable.update(insertTable.text.set("modified")).where(abstractBooleanExpression).execute(getEngine());
+        final List<Pair<Integer, String>> list = insertTable.id.pair(insertTable.text).orderBy(insertTable.id).list(getEngine());
+        assertEquals(Arrays.asList(
+                Pair.make(1, "one"),
+                Pair.make(2, "modified"),
+                Pair.make(3, "modified"),
+                Pair.make(4, "four"),
+                Pair.make(5, "modified")
+                ), list);
+    }
+
+}
--- symqle-0.1/symqle/src/test/java/org/symqle/integration/AggregatesTest.java
+++ symqle-0.1/symqle/src/test/java/org/symqle/integration/AggregatesTest.java
@@ -1,9 +1,12 @@
 package org.symqle.integration;
 
 import org.symqle.common.Pair;
-import org.symqle.sql.Params;
 import org.symqle.integration.model.Department;
 import org.symqle.integration.model.Employee;
+import org.symqle.sql.AbstractAggregateFunction;
+import org.symqle.sql.AggregateFunction;
+import org.symqle.sql.Params;
+import org.symqle.testset.AbstractAggregateFunctionTestSet;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -13,7 +16,340 @@ import java.util.List;
 /**
  * @author lvovich
  */
-public class AggregatesTest extends AbstractIntegrationTestBase {
+public class AggregatesTest extends AbstractIntegrationTestBase  implements AbstractAggregateFunctionTestSet {
+
+    @Override
+    public void test_adapt_AggregateFunction() throws Exception {
+        final Employee employee = new Employee();
+        final AggregateFunction<Integer> count = employee.empId.count();
+        final List<Integer> list = AbstractAggregateFunction.adapt(count).list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_compileQuery_QueryEngine_Option() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().compileQuery(getEngine()).list();
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_contains_Object() throws Exception {
+        final Department department = new Department();
+        final Employee employee = new Employee();
+        final List<String> list = department.deptName.where(employee.empId.count().contains(5)).
+                orderBy(department.deptName)
+                .list(getEngine());
+        assertEquals(Arrays.asList("DEV", "HR"), list);
+    }
+
+    @Override
+    public void test_distinct_() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().distinct().list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_exceptAll_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.gt(100.0)).
+                    exceptAll(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_exceptAll_QueryTerm() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    exceptAll(employee.empId.count().where(employee.salary.gt(100.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_exceptDistinct_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.gt(100.0)).
+                    exceptDistinct(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_exceptDistinct_QueryTerm() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    exceptDistinct(employee.empId.count().where(employee.salary.gt(100.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_except_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.gt(100.0)).
+                    except(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_except_QueryTerm() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    except(employee.empId.count().where(employee.salary.gt(100.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support EXCEPT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_exists_() throws Exception {
+        final Department department = new Department();
+        final Employee employee = new Employee();
+        final List<String> list = department.deptName
+                .where(employee.salary.sum().exists())
+                .orderBy(department.deptName)
+                .list(getEngine());
+        assertEquals(Arrays.asList("DEV", "HR"), list);
+    }
+
+    @Override
+    public void test_forReadOnly_() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().forReadOnly().list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_forUpdate_() throws Exception {
+            // it is unclear, what 'for update' means when selecting count. Lock the whole table? Nevertheless, some engines allow it.
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().forUpdate().list(getEngine());
+            assertEquals(Arrays.asList(5), list);
+        } catch (SQLException e) {
+            // derby: ERROR 42Y90: FOR UPDATE is not permitted in this type of statement.
+            // org.postgresql.util.PSQLException: ERROR: SELECT FOR UPDATE/SHARE is not allowed with aggregate functions
+            expectSQLException(e, "Apache Derby", "PostgreSQL");
+        }
+    }
+
+    @Override
+    public void test_in_Predicand_InPredicateValue_1() throws Exception {
+        final Employee employee = new Employee();
+        final Employee allEmployees = new Employee();
+        final List<String> list = employee.lastName.where(employee.empId.in(allEmployees.deptId.count())).list(getEngine());
+        assertEquals(Arrays.asList("Pedersen"), list);
+    }
+
+    @Override
+    public void test_intersectAll_QueryPrimary() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    intersectAll(employee.empId.count().where(employee.salary.lt(1800.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_intersectAll_QueryTerm_QueryPrimary_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.lt(1800.0)).
+                    intersectAll(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_intersectDistinct_QueryPrimary() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    intersectDistinct(employee.empId.count().where(employee.salary.lt(1800.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_intersectDistinct_QueryTerm_QueryPrimary_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.lt(1800.0)).
+                    intersectDistinct(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_intersect_QueryPrimary() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().
+                    intersect(employee.empId.count().where(employee.salary.gt(1800.0))).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_intersect_QueryTerm_QueryPrimary_1() throws Exception {
+        final Employee employee = new Employee();
+        try {
+            final List<Integer> list = employee.empId.count().where(employee.salary.gt(1800.0)).
+                    intersect(employee.empId.count()).list(getEngine());
+            assertEquals(0, list.size());
+        } catch (SQLException e) {
+            // mysql: does not support INTERSECT
+            expectSQLException(e, "MySQL");
+        }
+    }
+
+    @Override
+    public void test_limit_int() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().limit(1).list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_limit_int_int() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().limit(1, 1).list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_list_QueryEngine_Option() throws Exception {
+        final Employee employee = new Employee();
+        final List<Integer> list = employee.empId.count().list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_notIn_Predicand_InPredicateValue_1() throws Exception {
+        final Employee employee = new Employee();
+        final Employee allEmployees = new Employee();
+        final List<String> list = employee.lastName.where(employee.empId.notIn(allEmployees.deptId.count())).
+                orderBy(employee.lastName).
+                list(getEngine());
+        assertEquals(Arrays.asList("Cooper", "First", "March", "Redwood"), list);
+    }
+
+    @Override
+    public void test_orderBy_SortSpecification_SortSpecification() throws Exception {
+        final Employee employee = new Employee();
+        final AbstractAggregateFunction<Integer> count = employee.empId.count();
+        final List<Integer> list = count.orderBy(employee.empId).list(getEngine());
+        assertEquals(Arrays.asList(5), list);
+    }
+
+    @Override
+    public void test_queryValue_() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_scroll_QueryEngine_Callback_Option() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_selectAll_() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_showQuery_Dialect_Option() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_unionAll_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_unionAll_QueryTerm() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_unionDistinct_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_unionDistinct_QueryTerm() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_union_QueryExpressionBodyScalar_QueryTerm_1() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_union_QueryTerm() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void test_where_WhereClause() throws Exception {
+        // TODO implement
+        throw new RuntimeException("Not implemented");
+    }
 
     public void testSelect() throws Exception {
         final Employee employee = new Employee();
